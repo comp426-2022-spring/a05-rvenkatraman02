8fe9b79eb3eed67e43e4dab8b3ef9ea834a8353b
rvenkatraman02 <rvenka@ad.unc.edu>
2022-04-19 00:00:43 -0400

    Completed single and multi flips

--- public/main.js
+++ public/main.js
@@ -64,8 +64,86 @@ function guessActive() {
     multi.classList.add('hidden')
 }
 // Flip one coin and show coin image to match result when button clicked
+function coinFlip() {
+    fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
+  		.then(function(response) {
+    		return response.json()
+  	    })
+		.then(function(result) {
+			console.log(result)
+				document.getElementById("result").innerHTML = result.flip
+				document.getElementById("quarter").setAttribute("src", "/public/assets/img/"+result.flip+".png")
+				coin.disabled = true
+		})
+}
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+
+// Our flip many coins form
+const coins = document.getElementById("coin_input")
+// Add event listener for coins form
+coins.addEventListener("submit", flipCoins)
+// Create the submit handler
+async function flipCoins(event) {
+    event.preventDefault();
+    
+    const endpoint = "app/flip/coins/"
+    const url = document.baseURI+endpoint
+
+    const formEvent = event.currentTarget
+
+    try {
+        const formData = new FormData(formEvent)
+        const flips = await sendFlips({ url, formData })
+
+        console.log(flips)
+
+        // Put each result in a table
+        var flipResults = document.getElementById('fliprows')
+        for (let i = 1; i <= flips.raw.length; i++) {
+            // creates a table row
+            var row = document.createElement("tr")
+            var currFlip = document.createElement("td")
+            currFlip.innerHTML = i
+            row.appendChild(currFlip)
+            var currResult = document.createElement("td")
+            currResult.innerHTML = result.raw[i]
+            row.appendChild(currResult)
+            var currImage = document.createElement("td")
+            var currImage2 = document.createElement("img")
+            currImage2.setAttribute("src", "/public/assets/img/"+currResult+".png")
+            currImage.appendChild(currImage2)
+            row.appendChild(currImage)
+
+            flipResults.appendChild(row)
+        }
+
+        document.getElementById("heads").innerHTML = "Heads: "+flips.summary.heads
+        document.getElementById("tails").innerHTML = "Tails: "+flips.summary.tails
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// Create a data sender
+async function sendFlips({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJson = JSON.stringify(plainFormData);
+    console.log(formDataJson);
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: formDataJson
+    };
+
+    const response = await fetch(url, options);
+    return response.json()
+}
 
 // Guess a flip by clicking either heads or tails button
